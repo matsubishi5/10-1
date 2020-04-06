34cda4a838170a8a878deb7232e9967876ef48a3
TakumiMatsuo <user@matsuotakumi-no-MacBook-Air.local>
2020-04-06 20:52:40 +0900

    crontab

--- bookers2-debug/app/models/book.rb
+++ bookers2-debug/app/models/book.rb
@@ -12,16 +12,20 @@ class Book < ApplicationRecord
     end
 
     def Book.search(search, user_or_book, how_search)
-            if how_search == "1"
-                Book.where(['title LIKE ?', "#{search}"])
-            elsif how_search == "2"
-                Book.where(['title LIKE ?', "#{search}%"])
-            elsif how_search == "3"
-                Book.where(['title LIKE ?', "%#{search}"])
-            elsif how_search == "4"
-                Book.where(['title LIKE ?', "%#{search}%"])
-            else
-                Book.all
-            end
+        if how_search == "1"
+            Book.where(['title LIKE ?', "#{search}"])
+        elsif how_search == "2"
+            Book.where(['title LIKE ?', "#{search}%"])
+        elsif how_search == "3"
+            Book.where(['title LIKE ?', "%#{search}"])
+        elsif how_search == "4"
+            Book.where(['title LIKE ?', "%#{search}%"])
+        else
+            Book.all
         end
+    end
+
+    def self.create_test
+        Book.create(title: "test", body: "testtest")
+    end
 end
